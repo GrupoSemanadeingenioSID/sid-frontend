95875f30b021d675b24c096a2958f0655c42fb8d
StexRoles <navarro456123@gmail.com>
2025-05-11 18:00:33 -0500

    ADD: estructura de etiquetas para el home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,42 @@
+import { lilita } from "./layout";
 
 export default function Home() {
   return (
-    <main className="">
-      
-    </main>
+    <div>
+        <header></header> {/* ESPACIO PARA EL COMPONENTE HEADER */}
+
+        <main>
+          {/* SECCION PARA EL BANNER */}
+          <section>
+
+          </section>
+
+          {/* SECCION PARA EL CONTENIDO */}
+          <section>
+            {/* ARTICULO PARA LAS ACTIVIDADES */}
+            <article>
+
+            </article>
+
+            {/* ARTICULO PARA LOS CURSOS */}
+            <article>
+
+            </article>
+
+            {/* ARTICULO PARA LOS PROYECTOS */}
+            <article>
+
+            </article>
+
+            {/* ARTICULO PARA LOS EQUIPOS*/}
+            <article>
+
+            </article>
+          </section>
+
+        </main>
+
+        <footer></footer> {/* ESPACIO PARA EL COMPONENTE FOOTER */}
+    </div>
   );
 }
