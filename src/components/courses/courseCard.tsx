c246147d69d578bda4b2a58f2ed47c61a8293e65
StexRoles <navarro456123@gmail.com>
2025-05-23 17:26:34 -0500

    FIX: se corrigio la altura de las tarjetas y el tamaño de la categoria

--- src/components/courses/courseCard.tsx
+++ src/components/courses/courseCard.tsx
@@ -2,14 +2,14 @@ import { Timer } from 'lucide-react';
 
 export default function CourseCard() { 
     return (
-        <article className="flex flex-col h-72 w-full hover:scale-105 transition-all gap-2.5 bg-french-gray rounded-3xl p-2.5 md:w-[30%] lg:h-80 lg:w-[28%]">
+        <article className="flex flex-col h-auto w-full hover:scale-105 transition-all gap-2.5 bg-french-gray rounded-3xl p-2.5 md:w-[30%]  lg:w-[28%]">
             {/* IMAGEN DEL CURSO */}
             <div className="h-1/2 w-auto rounded-3xl overflow-hidden">
                 <img src="/images/course/diseño.webp" className="w-full h-full bg-cover" alt="Course Image" />
             </div>
 
             {/* CONTENIDO DEL CURSO */}
-            <div className='h-1/2 flex flex-col justify-between p-2.5'>
+            <div className='h-1/2 flex flex-col justify-between p-2.5 gap-2'>
                 {/* PROFESOR DEL CURSO */}
                 <div className="flex items-center justify-between">
                     <div className="h-6 w-6 rounded-full overflow-hidden lg:h-10 lg:w-10">
@@ -18,8 +18,8 @@ export default function CourseCard() {
                     
                     <p className="font-bold text-xs text-center text-davys-gray lg:text-sm">Richard Eduardo</p>
 
-                    <div className="bg-lapiz-lazuli rounded-md font-semibold px-2.5 py-1.5 flex items-center justify-center text-center text-xs lg:text-sm">
-                        <p>Tecnología</p>
+                    <div className="bg-lapiz-lazuli rounded-md font-semibold px-2.5 py-1.5 flex items-center justify-center text-center w-1/3 text-[clamp(0.5rem,1.2vw,0.85rem)]">
+                      <p className="truncate w-full">Tecnología</p>
                     </div>
                 </div>
 
