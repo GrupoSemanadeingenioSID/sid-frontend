cb9b7313d6e6b693ea1e52d886ab27eac43cb091
PilarPradilla <cely9596@gmail.com>
2025-05-26 17:47:37 -0500

    CREATE: crear actividades en el home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,101 @@ export default function Home() {
         <section className="flex flex-col py-5 px-10 gap-5 lg:px-40 bg-[url(/images/background.png)] bg-cover bg-no-repeat">
           {/* ARTICULO PARA LAS ACTIVIDADES */}
           <article>
+            <h2 className="w-full text-center text-2xl font-bold lg:text-3xl">Actividades Programadas</h2>
+            
+            <div className="w-full max-w-screen-xl flex flex-wrap justify-between gap-x-2 gap-y-2 max-[380px]:gap-x-1 max-[380px]:gap-y-1 py-4">
 
+              <div className="w-[8.75rem] sm:w-full sm:max-w-[15rem] h-[198px] sm:h-[340px] flex flex-col justify-between items-center gap-2 rounded-lg overflow-hidden shadow-md bg-black/50 py-2 relative transition-transform duration-300 hover:scale-105">
+                <img 
+                  src="/images/activity/lectura.webp" 
+                  alt="Lectura" 
+                  className="absolute inset-0 w-full h-full object-cover z-0"
+                />
+                <div className="absolute bottom-0 left-0 w-full h-[40%] z-0" 
+                  style={{
+                    background: "linear-gradient(0deg, rgba(0, 0, 0, 0.9) 0%, rgba(0, 0, 0, 0.45) 50%, rgba(0, 0, 0, 0) 100%)"
+                  }}></div>
+                  
+                <div className="relative z-10 w-full flex flex-col items-center justify-end mt-auto gap-1 px-2 my-4 space-y-2 rounded-lg sm:my-8 sm:space-y-4
+                  sm:mt-auto sm:mb-8">
+                  <h3 className="text-center text-[#CED5DC] text-lg md:text-2xl font-bold font-['Nunito'] leading-none">
+                    Lectura
+                  </h3>
+                  <p className="text-center text-[#CED5DC] text-sm font-normal font-['Nunito'] leading-none text-[10px] sm:text-sm">
+                    Reunión para leer clásico literatura
+                  </p>
+                </div>
+              </div>
+
+              <div className="w-[8.75rem] sm:w-full sm:max-w-[15rem] h-[198px] sm:h-[340px] flex flex-col justify-between items-center gap-2 rounded-lg overflow-hidden shadow-md bg-black/50 py-2 relative transition-transform duration-300 hover:scale-105">
+                <img 
+                  src="/images/activity/seguridad.webp" 
+                  alt="Ciberseguridad" 
+                  className="absolute inset-0 w-full h-full object-cover z-0"
+                />
+                <div className="absolute bottom-0 left-0 w-full h-[40%] z-0" 
+                  style={{
+                    background: "linear-gradient(0deg, rgba(0, 0, 0, 0.9) 0%, rgba(0, 0, 0, 0.45) 50%, rgba(0, 0, 0, 0) 100%)"
+                  }}></div>
+                  
+                <div className="relative z-10 w-full flex flex-col items-center justify-end mt-auto gap-1 px-2 my-4 space-y-2 rounded-lg sm:my-8 sm:space-y-4
+                  sm:mt-auto sm:mb-8">
+                  <h3 className="text-center text-[#CED5DC] text-lg md:text-2xl font-bold font-['Nunito'] leading-none">
+                    Ciberseguridad
+                  </h3>
+                  <p className="text-center text-[#CED5DC] text-sm font-normal font-['Nunito'] leading-none text-[10px] sm:text-sm">
+                    Taller corto sobre ciberseguridad en la universidad
+                  </p>
+                </div>
+              </div>
+
+              <div className="w-[8.75rem] sm:w-full sm:max-w-[15rem] h-[198px] sm:h-[340px] flex flex-col justify-between items-center gap-2 rounded-lg overflow-hidden shadow-md bg-black/50 py-2 relative transition-transform duration-300 hover:scale-105">
+                <img 
+                  src="/images/activity/ilustracion.webp" 
+                  alt="Ilustración" 
+                  className="absolute inset-0 w-full h-full object-cover z-0"
+                />
+
+                <div className="absolute bottom-0 left-0 w-full h-[40%] z-0" 
+                  style={{
+                    background: "linear-gradient(0deg, rgba(0, 0, 0, 0.9) 0%, rgba(0, 0, 0, 0.45) 50%, rgba(0, 0, 0, 0) 100%)"
+                }}></div>
+                  
+                <div className="relative z-10 w-full flex flex-col items-center justify-end mt-auto gap-1 px-2 my-4 space-y-2 rounded-lg sm:my-8 sm:space-y-4
+                  sm:mt-auto sm:mb-8">
+                  <h3 className="text-center text-[#CED5DC] text-lg md:text-2xl font-bold font-['Nunito'] leading-none">
+                    Ilustración
+                  </h3>
+                  <p className="text-center text-[#CED5DC] text-sm font-normal font-['Nunito'] leading-none text-[10px] sm:text-sm">
+                    Taller de ilustración realista para el diseño gráfico
+                  </p>
+                </div>
+              </div>
+
+              <div className="w-[8.75rem] sm:w-full sm:max-w-[15rem] h-[198px] sm:h-[340px] flex flex-col justify-between items-center gap-2 rounded-lg overflow-hidden shadow-md bg-black/50 py-2 relative transition-transform duration-300 hover:scale-105">
+                <img 
+                  src="/images/activity/fotografia.webp" 
+                  alt="Fotografía" 
+                  className="absolute inset-0 w-full h-full object-cover z-0"
+                />
+
+                <div className="absolute bottom-0 left-0 w-full h-[40%] z-0" 
+                  style={{
+                    background: "linear-gradient(0deg, rgba(0, 0, 0, 0.9) 0%, rgba(0, 0, 0, 0.45) 50%, rgba(0, 0, 0, 0) 100%)"
+                  }}></div>
+
+                <div className="relative z-10 w-full flex flex-col items-center justify-end mt-auto gap-1 px-2 my-4 space-y-2 rounded-lg sm:my-8 sm:space-y-4
+                  sm:mt-auto sm:mb-8">
+                  <h3 className="text-center text-[#CED5DC] text-lg md:text-2xl font-bold font-['Nunito'] leading-none">
+                    Fotografía
+                  </h3>
+                  <p className="text-center text-[#CED5DC] text-sm font-normal font-['Nunito'] leading-none text-[10px] sm:text-sm">
+                    Taller de fotografías a paisajes de la ciudad
+                  </p>
+                </div>
+              </div>
+              
+            </div>
           </article>
 
           {/* ARTICULO PARA LOS CURSOS */}
