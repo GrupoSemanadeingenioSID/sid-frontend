5fce65fa0e9b20adf978a787d093d4f413d19951
CarlosGongorar <carlosgongorar50@gmail.com>
2025-05-26 00:30:52 -0500

    FIX: Corrigiendo Responsivity

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -24,36 +24,38 @@ export default function Header() {
     }, [])
     
     return (
-        <header className="border border-black flex">
+        <header className="border border-black flex justify-between items-center px-4 py-2 w-full">
             {/* Logos */}    
-            <div className="mx-auto px-5 flex items-center">
-                <Image src="/images/logotype/Escudo_UD.svg.png" alt="EscudoUD" width={65} height={65}/>
+            <div className="px-5 flex items-center space-x-1">
+                <Image src="/images/logotype/Escudo_UD.svg.png" alt="EscudoUD" width={65} height={65} className="w-20 sm:w-30 md:w-30 lg:w-30 h-auto object-contain"/>
                 <span className='text-black text-5xl font-extralight'>|</span>
-                <Image src="/images/logotype/logosid.webp" alt="LogoSid" width={65} height={65}/>
+                <Image src="/images/logotype/logosid.webp" alt="LogoSid" width={65} height={65} className="w-20 sm:w-30 md:w-30 lg:w-30 h-auto object-contain"/>
             </div>
             {/**Menu PC */}
-            <div className="hidden md:flex space-x-4">
-                <button className="border px-3 py-1 rounded hover:bg-gray-200">Opción 1</button>
-                <button className="border px-3 py-1 rounded hover:bg-gray-200">Opción 2</button>
-                <button className="border px-3 py-1 rounded hover:bg-gray-200">Opción 3</button>
-            </div>
-            <div className="flex justify-between items-center px-10 w-50 max-w-xs mx-auto">
+            <nav className="hidden xl:flex space-x-6 py-2 xl:px-20 rounded-4xl border m-3">
+                <button className="text-lg lg:text-2xl px-3 font-bold text-gray-500">Inicio</button>
+                <button className="text-lg lg:text-2xl px-3 font-bold text-gray-500">Quienes somos</button>
+                <button className="text-lg lg:text-2xl px-3 font-bold text-gray-500">Cursos</button>
+                <button className="text-lg lg:text-2xl px-3 font-bold text-gray-500">Equipos</button>
+                <button className="text-lg lg:text-2xl px-3 font-bold text-gray-500">Aliados</button>
+            </nav>
+
+            <div className="flex justify-between items-center px-10 w-50">
                 {/** Boton de notificacion */} 
-                <div className="flex border rounded-full p-2">
+                <div className="flex border rounded-full p-2 m-1">
                     <button onClick={()=>{setIsNotiOpen(!isNotiOpen)}}>
                         <Image src="/images/icons/bell.svg" alt='menu' height={20} width={20}/>
                     </button>
                 </div>
                 {/**Boton desplegable de movil*/}
-                <div className="relative md:hidden">
+                <div className="relative xl:hidden m-1">
                     <button onClick={()=>{setIsMenuOpen(!isMenuOpen)}}>
                         <Image src="/images/icons/menu.svg" alt='menu' height={30} width={30}/>
                     </button>
                 </div>
             </div>
-
             {/** Desplegable Noti Celular */}
-            <div className={`fixed left-0 top-0 h-full w-full bg-[url('/images/background-menu.png')] bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center md:hidden ${isNotiOpen ? 'translate-x-0 pointer-events-auto' : '-translate-x-full pointer-events-none'} transform transition-transform duration-250 ease-in-out`}>
+            <div className={`fixed left-0 top-0 h-full w-full bg-[url('/images/background-menu.png')] bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center xl:hidden ${isNotiOpen ? 'translate-x-0 pointer-events-auto' : '-translate-x-full pointer-events-none'} transform transition-transform duration-250 ease-in-out`}>
                 <div ref={notiRef} className="w-full h-full p-5 flex flex-col gap-4 items-center">
                     {/** Boton de cerrado */}
                         <button className="self-end text-2xl font-bold mb-4 text-white" onClick={()=>setIsNotiOpen(false)}>
@@ -67,7 +69,7 @@ export default function Header() {
             </div>
 
             {/** Desplegable Menu Celular */}
-                <div className={`fixed right-0 top-0 h-full w-full bg-[url('/images/background-menu.png')] bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center md:hidden ${isMenuOpen ? 'translate-x-0 pointer-events-auto' : 'translate-x-full pointer-events-none'} transform transition-transform duration-250 ease-in-out`}>
+                <div className={`fixed right-0 top-0 h-full w-full bg-[url('/images/background-menu.png')] bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center xl:hidden ${isMenuOpen ? 'translate-x-0 pointer-events-auto' : 'translate-x-full pointer-events-none'} transform transition-transform duration-250 ease-in-out`}>
                     <div ref={menuRef} className="w-full h-full p-5 flex flex-col gap-4 items-center">
                         {/** Boton de cerrado */}
                         <button className="self-end text-2xl font-bold mb-4 text-white" onClick={()=>setIsMenuOpen(false)}>
