939c59755a9cd093e845b26404d529232aac494e
CarlosGongorar <carlosgongorar50@gmail.com>
2025-06-01 23:09:01 -0500

    UPDATE: Terminando diseño y glassmorfismo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -28,7 +28,7 @@ export default function Header() {
     }, [])
     
     return (
-        <header className="border border-black flex justify-between items-center px-4 py-2 w-full">
+        <header className="fixed top-0 left-0 flex justify-between items-center px-4 py-2 w-full">
             {/* Logos */}    
             <div className="mx-auto px-5 flex items-center space-x-1">
                 <Image src="/images/logotype/Escudo_UD.svg.png" alt="EscudoUD" width={65} height={65} className="w-20 sm:w-30 md:w-30 lg:w-30 h-auto object-contain"/>
@@ -36,28 +36,32 @@ export default function Header() {
                 <Image src="/images/logotype/logosid.webp" alt="LogoSid" width={65} height={65} className="w-20 sm:w-30 md:w-30 lg:w-30 h-auto object-contain"/>
             </div>
             {/**Menu PC */}
-            <nav className="hidden xl:flex space-x-6 py-4 xl:px-20 rounded-4xl border m-4">
-                <button className="text-lg lg:text-2xl px-3 font-bold text-gray-500 cursor-pointer">Inicio</button>
-                <button className="text-lg lg:text-2xl px-3 font-bold text-gray-500 cursor-pointer">Quienes somos</button>
-                <button className="text-lg lg:text-2xl px-3 font-bold text-gray-500 cursor-pointer">Cursos</button>
-                <button className="text-lg lg:text-2xl px-3 font-bold text-gray-500 cursor-pointer">Equipos</button>
-                <button className="text-lg lg:text-2xl px-3 font-bold text-gray-500 cursor-pointer">Aliados</button>
+            <nav className="hidden xl:flex space-x-6 py-4 xl:px-20 rounded-4xl m-4 bg-white/30 backdrop-blur-md shadow-2xl">
+                <button className="text-lg lg:text-2xl px-3 font-bold text-gray-900 cursor-pointer">Inicio</button>
+                <button className="text-lg lg:text-2xl px-3 font-bold text-gray-900 cursor-pointer">Quienes somos</button>
+                <button className="text-lg lg:text-2xl px-3 font-bold text-gray-900 cursor-pointer">Cursos</button>
+                <button className="text-lg lg:text-2xl px-3 font-bold text-gray-900 cursor-pointer">Equipos</button>
+                <button className="text-lg lg:text-2xl px-3 font-bold text-gray-900 cursor-pointer">Aliados</button>
             </nav>
             
             {/** Boton Notis de Escritorio */}
-            <div className="relative hidden xl:block ml-10">
-                <div className="w-15 h-15 flex items-center justify-center rounded-full border cursor-pointer mx-auto" onClick={() => setIsDesktopNotiOpen(!isDesktopNotiOpen)}>
+            <div className="relative hidden xl:block ml-10 ">
+                <div className="w-15 h-15 flex items-center justify-center rounded-full cursor-pointer mx-auto bg-white/30 backdrop-blur-md shadow-2xl" onClick={() => setIsDesktopNotiOpen(!isDesktopNotiOpen)}>
                     <Image src="/images/icons/bell.svg" alt="notificación" width={40} height={40}/>
                 </div>
             
-
+            {/** Menu desplegable de PC 
+             * 1. Toca revisar el diseño
+             * Pasarle una lista aun componente que renderise las notificaciones
+            */}
+            
             {isDesktopNotiOpen && (
-            <div className="absolute right-0 mt-2 w-64 bg-white border rounded shadow-lg z-50">
-                <div className="p-4 font-bold border-b">Notificaciones</div>
+            <div className="absolute right-0 mt-2 w-64 border rounded z-50 bg-white/30 backdrop-blur-md shadow-2xl">
+                <div className="p-4 font-bold border-b text-gray-900">Notificaciones</div>
                 <ul className="p-2 space-y-2 text-sm">
-                    <li className="hover:bg-gray-100 p-2 rounded">Tienes una nueva alerta</li>
-                    <li className="hover:bg-gray-100 p-2 rounded">Tu curso ha sido actualizado</li>
-                    <li className="hover:bg-gray-100 p-2 rounded">Consulta tus mensajes</li>
+                    <li className="hover:bg-gray-100 p-2 rounded text-gray-900">Tienes una nueva alerta</li>
+                    <li className="hover:bg-gray-100 p-2 rounded text-gray-900">Tu curso ha sido actualizado</li>
+                    <li className="hover:bg-gray-100 p-2 rounded text-gray-900">Consulta tus mensajes</li>
                 </ul>
             </div>
             )}
@@ -65,19 +69,20 @@ export default function Header() {
 
             <div className="flex justify-between items-center px-10 w-50 mx-auto">
                 {/** Boton de notificacion */} 
-                <div className="relative xl:hidden flex border rounded-full p-2 m-1">
+                <div className="relative xl:hidden flex rounded-full p-2 m-1 w-15 h-15 justify-center bg-white/30 backdrop-blur-md shadow-2xl">
                     <button onClick={()=>{setIsNotiOpen(!isNotiOpen)}} className='cursor-pointer'>
-                        <Image src="/images/icons/bell.svg" alt='menu' height={20} width={20}/>
+                        <Image src="/images/icons/bell.svg" alt='menu' height={40} width={40}/>
                     </button>
                 </div>
 
-                {/**Boton desplegable de movil*/}
+                {/**Boton menu desplegable de movil*/}
                 <div className="relative xl:hidden m-1">
                     <button onClick={()=>{setIsMenuOpen(!isMenuOpen)}} className='cursor-pointer'>
                         <Image src="/images/icons/menu.svg" alt='menu' height={30} width={30}/>
                     </button>
                 </div>
-                <button className="hidden xl:flex border rounded-4xl p-4 px-5 mx-auto text-2xl whitespace-nowrap cursor-pointer">
+                {/** Boton de Inicio de Sesion */}
+                <button className="hidden xl:flex border-4 text-black font-extrabold border-black rounded-4xl p-4 px-5 mx-auto text-2xl whitespace-nowrap cursor-pointer bg-white/20 backdrop-blur-md shadow-2xl">
                     Iniciar Sesion
                 </button>
             </div>
@@ -89,6 +94,9 @@ export default function Header() {
                         <button className="self-end text-2xl font-bold mb-4 text-white" onClick={()=>setIsNotiOpen(false)}>
                             X
                         </button>
+                        {/** Para este apartado se puede
+                         * 1. Hacer que se le pase una lista a un componente y lo renderice con un .map
+                         */}
                         <Image src="/images/logotype/bluelogo.webp" alt="LogoSidBlue" height={150} width={150} className='py-10'/>
                         <h2>Notificacion 1</h2>
                         <h2>Notificacion 2</h2>
@@ -103,7 +111,9 @@ export default function Header() {
                         <button className="self-end text-2xl font-bold mb-4 text-white" onClick={()=>setIsMenuOpen(false)}>
                             X
                         </button>
-
+                        {/** Aca cada uno tiene que redirigir a otros lados de la pagina 
+                         * Se puede hacer un componente boton unico para cada uno
+                        */}
                         <Image src="/images/logotype/bluelogo.webp" alt="LogoSidBlue" height={150} width={150} className='py-10'/>
                         <button onClick={() => setIsMenuOpen(false)} className="text-xl py-2 border border-[#135e8c] px-4 text-center rounded-3xl text-[#135e8c] font-bold">
                             Iniciar Sesión
