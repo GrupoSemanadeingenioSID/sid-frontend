fd79078797cb495d2ecfc8c282fc5dfbcf8c10c6
PilarPradilla <cely9596@gmail.com>
2025-06-07 18:52:52 -0500

    UPDATE: se mejoro la seccion de actividades en el main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,23 +34,23 @@ export default function Home() {
             <h2 className="w-full text-center text-2xl font-bold lg:text-3xl">Actividades Programadas</h2>
             
             <div className="flex flex-row flex-wrap gap-4 mt-5 justify-around">
-
               {activities.map((activity, index) => (
-                <div key={index} className="w-full min-h-49 flex items-end rounded-lg relative overflow-hidden max-w-[45%] md:max-w-[22%] md:min-h-75 lg:min-h-83 xl:min-w-57 shadow-md bg-black/50 py-2 transition-transform duration-300 hover:scale-105">
-                  <a key={index} href="" target="_blank" rel="noopener noreferrer">
-                    <img
-                    src="/images/activity/ilustracion.webp"
-                    alt="Lectura"
-                    className="absolute inset-0 w-full h-full object-cover z-0"
-                    />
-                    <div className="absolute bottom-0 z-10 w-full flex flex-col bg-gradient-to-t from-black/90 via-black/45 to-transparent mt-auto p-3 pt-3">
-                      <h3 className="text-center text-[#CED5DC] font-bold font-nunito md:text-xl lg:text-2xl">Lectura</h3>
-                      <p className="text-center text-[#CED5DC] font-normal font-nunito text-[10px] md:text-[14px]">Reunión para leer clasico literatura</p>
-                    </div>
-                  </a>
-                </div>
+                <Link key={index} href={""}
+                  className="w-full flex items-end rounded-lg relative overflow-hidden max-w-[45%] md:max-w-[23%] aspect-[3/4] shadow-md bg-black/50 py-2 transition-transform duration-300 hover:scale-105"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                  src="/images/activity/ilustracion.webp"
+                  alt="Lectura"
+                  className="absolute inset-0 w-full h-full object-cover z-0"
+                  />
+                  <div className="absolute bottom-0 z-10 w-full flex flex-col bg-gradient-to-t from-black/90 via-black/45 to-transparent mt-auto p-3 pt-3">
+                  <h3 className="text-center text-[#CED5DC] font-bold font-nunito md:text-xl lg:text-2xl">Lectura</h3>
+                  <p className="text-center text-[#CED5DC] font-normal font-nunito text-[10px] md:text-[14px]">Reunion para leer clasico literatura</p>
+                  </div>
+                </Link>
               ))}
-
             </div>
             
           </article>
