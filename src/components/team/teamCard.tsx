bbd9badbd35b5265bad93b5648e1fcb115e22149
IngPipex06 <fellippex06@hotmail.com>
2025-07-18 17:13:45 -0500

    CREATE: Sección del Equipo (con carrusel, responsividad y estilos)

--- src/components/team/teamCard.tsx
+++ src/components/team/teamCard.tsx
@@ -1,24 +1,23 @@
-import Image from 'next/image'
-import member from "../../../.next/static/types.js";
+import Image from 'next/image';
+type member = {
+  nombre: string;
+  rol: string;
+  foto: string;
+  alt?: string;
+};
 
 export default function TeamCard({nombre, rol, foto, alt }: member): React.JSX.Element {
 	return(
-		<article className="flex flex-col bg-[var(--russian-violet)] rounded-[200px]
-             justify-between
-             w-[clamp(130px,min(36.842vw,46%),150px)] md:w-[clamp(175px,18.125%,225px)] xl:w-[clamp(250px,15vw,300px)]
-             h-[15.75rem] md:h-[clamp(315px,22.5vw,360px)] xl:h-[clamp(300px,25vw,400px)]
-             gap-2.5
-             shrink-0 mx-[1vw]">
+		<article   className="flex flex-col bg-[var(--russian-violet)] rounded-[200px] justify-between w-35 sm:w-38 md:w-42 lg:w-48.5 xl:w-50 h-60 sm:h-66.5 md:h-73 lg:h-79.5 xl:h-90 gap-2.5 shrink-0 mx-[1vw] min-h-[225px]">
 
 			{/* INFORMACIÓN DEL INTEGRANTE */}
 			<header className="flex flex-col justify-center items-center flex-grow gap-y-5">
-				<h2 className="font-nunito font-bold text-[16px] leading-[100%] tracking-normal text-[var(--french-gray)] "> {nombre} </h2>
-				<p className="font-nunito font-medium text-[12px] leading-[100%] tracking-normal text-[var(--french-gray)]"> {rol} </p>
+				<h2 className="max-w-1/2 break-words font-nunito text-center font-bold text-base leading-[100%] tracking-normal text-[var(--french-gray)] lg:text-lg" > {nombre} </h2>
+				<p className="font-nunito font-medium text-xs leading-[100%] tracking-normal text-[var(--french-gray)] lg:text-base"> {rol} </p>
 			</header>
 
       {/* IMAGEN DEL INTEGRANTE */}
-			<figure className="w-full h-[clamp(140px,16rem,9rem)] md:h-[clamp(140px,12rem,252px)] xl:h-[clamp(140px,15vw,252px)]
-                        rounded-[200px] relative object-cover aspect-square sm:aspect-2/3">
+			<figure className="w-full h-38 sm:h-44 md:h-50 lg:h-55  xl:h-62 rounded-[200px] relative object-cover aspect-square sm:aspect-2/3">
 				<Image
 					src={foto}
 					alt={alt ?? `Foto de ${nombre}`}
